2acb41637dae21875a964d083b452585a6eb17cf
saryDong <2997723775@qq.com>
2019-12-30 16:18:11 +0800

    Initial commit

---
+++ app/src/main/java/com/abu/androidutils/MainActivity.java
@@ -0,0 +1,19 @@
+package com.abu.androidutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @date: 2019/12/30 16:13
+ * @author: 董长峰
+ * @blog: https://www.jianshu.com/u/04a705fae99b
+ * @description:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
